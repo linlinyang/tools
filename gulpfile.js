7095a8899308580ff6b885071d30d911b4f4215a
linlinyang <935704186@qq.com>
2018-06-29 11:22:56 +0800

    clear

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
-  console.log('Hello Zoro');
-});
