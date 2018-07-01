4e99246b3e8bfdeed44458d4a9329ac0ff7e8c4c
linlinyang <935704186@qq.com>
2018-07-01 14:40:28 +0800

    nodeIndex add

---
+++ src/dom/nodeIndex.js
@@ -0,0 +1,18 @@
+function nodeIndex(node){
+	var parent = node.parentNode,
+		pos = -1;
+	if(!parent){ return pos; }
+
+	var currNode = parent.firstChild;
+	while(currNode){
+		currNode.nodeType == 1 && pos++;
+		if(currNode == node){return pos; }
+		(currNode = currNode.nextSibling) && currNode.nodeType == 1 && pos++;
+		if(currNode == node){return pos; }
+		currNode = currNode.nextSibling;
+	}
+
+	return pos;
+}
+
+export default nodeIndex;
