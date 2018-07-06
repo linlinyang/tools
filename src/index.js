0d6f246eb997ee0e06f33fe695964d85192f58f1
linlinyang <935704186@qq.com>
2018-07-06 09:53:26 +0800

    clear local

--- src/index.js
+++
@@ -1,5 +0,0 @@
-//import isbase from "./ecma/isbase";
-//import isArray from "./ecma/isArray";
-//import repeatStr from "./ecma/repeatStr";
-import splitContain from "./ecma/splitContain";
-import toUrlParams from "./ecma/toUrlParams";
