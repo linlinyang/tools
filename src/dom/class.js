ff8c6d36c514302d6190e48e01c0a41c9cf41603
linlinyang <935704186@qq.com>
2018-07-18 22:36:58 +0800

    add class function

--- src/dom/class.js
+++ src/dom/class.js
@@ -1,24 +1,112 @@
 import trim from '../ecma/string/trim';
+import {win,doc,defaultView,rootEl} from './config.js';
 
 var div = document.createElement('div'),
-	contain = 'classList' in div 
+	hasClassList = 'classList' in div,
+	contain = hasClassList 
 					? function(el,name){
 						return el.classList.contains(name);
 					}
-					:function(el,name){
+					: function(el,name){
 						var reg = new RegExp('(^\|\\s\+)' + name + '(\\s\+\|$)','g');
 						return reg.test(el.className);
+					},
+	remove = hasClassList
+					? function(el,name){
+						el.classList.remove(name);
+					}
+					: function(el,name){
+						var reg = new RegExp('(^\|\\s\+)' + name + '(\\s\+\|$)','g'),
+							newClassName = el.className.replace(reg,"$2");;
+						el.className = newClassName;
 					};
 div = null;
+
 export function hasClass(el,className){
+	if(!el || el.nodeType !== 1){
+		throw new Error('just element node can has class');
+	}
+	if(typeof className !== 'string'){
+		throw new Error('Class name must be string');
+	}
+	if(el === win || el === doc){
+		return false;
+	}
 	var classNames = trim(className).split(/\s+/g),
 		len = classNames.length;
 
-	for(;len--;){
+	while(len--){
 		if(!contain(el,classNames[len])){
 			return false;
 		}
 	}
 	return true;
+}
+
+export function addClass(el,className){
+	if(!el || el.nodeType !== 1){
+		throw new Error('just element node can has class');
+	}
+	if(typeof className !== 'string'){
+		throw new Error('Class name must be string type');
+	}
+	if(el === win || el === doc){
+		return ;
+	}
+	className = trim(className);
+	if(hasClassList){
+		var classNames = className.split(/\s+/g),
+			len = classNames.length;
+		while(len--){
+			el.classList.add(classNames[len]);
+		}
+	}else{
+		el.className += ' ' + className;
+	}
+}
+
+export function removeClass(el,className){
+	if(!el || el.nodeType !== 1){
+		throw new Error('just element node can has class');
+	}
+	if(typeof className !== 'string'){
+		throw new Error('Class name must be string type');
+	}
+	if(el === win || el === doc){
+		return ;
+	}
+	var classNames = trim(className).split(/\s+/g),
+		len = classNames.length;
+	while(len--){
+		remove(el,classNames[len]);
+	}
+
+}
+
+export function toggleClass(el,className){
+	if(!el || el.nodeType !== 1){
+		throw new Error('just element node can has class');
+	}
+	if(typeof className !== 'string'){
+		throw new Error('Class name must be string type');
+	}
+	if(el === win || el === doc){
+		return ;
+	}
+	var classNames = trim(className).split(/\s+/g),
+		len = classNames.length;
+	while(len--){
+		var curClass = classNames[len];
+		if(hasClassList){
+			el.classList.toggle(curClass);
+		}else{
+			if(hasClass(el,curClass)){
+				remove(el,curClass);
+			}else{
+				el.className += ' ' + curClass;
+			}
+		}
+	}
+
 }
 
